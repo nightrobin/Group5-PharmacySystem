b7205bad7007e8c4832dc376a5a5943813d24623
aUrelogs <jplmaceda2021@plm.edu.ph>
2021-12-21 13:29:09 +0800

    Added new 3 minor features
    Added and displayed 3 minor features which is;
    
    to show the name of pharmacy
    to show the location of pharmacy
    to show the date and time

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello country!" << endl;
-    return 0;
-}
+   time_t timetoday;
+   time (&timetoday);
+
+    cout << "\t-----------------------------------------------------------------" << endl;
+    cout << "\t|Welcome to PLM - PHARMACY NG LUNGSOD NG MAYNILA \t\t|" << endl;
+    cout << "\t|Located at: Gen. Luna cor Muralla Sts., Intramuros, Manila \t|" << endl;
+    cout << "\t|CURRENT DATE & TIME: " << asctime(localtime(&timetoday));
+    cout << "\t-----------------------------------------------------------------" << endl;
+
+   return 0;
+
+   }
+
