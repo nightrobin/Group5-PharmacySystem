8b08dab93f302dab5af712ac5afd65da6de04809
aUrelogs <jplmaceda2021@plm.edu.ph>
2022-01-08 00:50:20 +0800

    ADDED DECLARATIONS

--- main.cpp
+++ main.cpp
@@ -28,6 +28,7 @@ int main()
     cout << "\t\t|[4] - ORDER LIST \t\t\t|" << endl;
     cout << "\t\t|[5] - DAILY SUMMARY\t\t\t|" << endl;
     cout << "\t\t|[6] - TOTAL PURCHASE \t\t\t|" << endl;
+    cout << "\t\t|[7] - E-PRESCRIPTION \t\t\t|" << endl;
     cout << "\t\t|[0] - EXIT \t\t\t\t|" << endl;
     cout << "\t\t-----------------------------------------" << endl;
 
@@ -41,22 +42,30 @@ int main()
         switch (choice){
 
             case 1: {
+                    pharmX.take_order();
                     break; }
             case 2: {
+                    pharmX.delete_order();
                     break; }
             case 3: {
+                    pharmX.modify();
                     break; }
             case 4: {
+                    pharmX.order_list();
                     break; }
             case 5: {
+                    pharmX.daily_summary();
                     break; }
             case 6: {
+                    pharmX.total_purchase();
                     break; }
+            case 7: {
+                    pharmX.e_prescription();
+
+                    break;}
             case 0: {
                     pharmX.exit();
-
                     break; }
-
 
 
         }
--- src/pharm_methods.cpp
+++ src/pharm_methods.cpp
@@ -10,6 +10,36 @@ pharm_methods::~pharm_methods()
 {
     //dtor
 }
+void pharm_methods::take_order (){
+
+}
+
+void pharm_methods::delete_order(){
+
+}
+
+void pharm_methods::modify(){
+
+}
+
+void pharm_methods::daily_summary(){
+
+}
+void pharm_methods::order_list(){
+
+}
+
+void pharm_methods::total_purchase(){
+
+}
+
+void pharm_methods::e_prescription() {
+
+    cout << "Please enter kind of desease or pain you are having";
+
+
+}
+
 void pharm_methods::exit(){
 
 
